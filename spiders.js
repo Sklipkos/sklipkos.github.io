45b62df2b55301e070a920166838160b233c003a
Sklipkos <sklipkos@gmail.com>
2026-08-11 20:05:23 +0200

    website-update

--- spiders.js
+++ spiders.js
@@ -1,182 +1,140 @@
 const spiders = [
-    {
+{
     name: "Aspinochilus (ex. Phormingochilus) rufus",
     sex: "Samice",
     size: "sub-adult",
     price: "500 Kč",
-    
     continent: "Asie"
-  },
-  {
+},
+
+{
     name: "Chilobrachys sp. Vietnam Blue",
     sex: "Samice",
     size: "adult",
     price: "1200 Kč",
-    
     continent: "Asie"
-  },
+},
 
-  {
-    name: "Phormictopus sp. Green",
-    sex: "Samice",
-    size: "5.5 cm",
-    price: "1000 Kč",
-    
-    continent: "Amerika"
-  },
-
-  {
-    name: "Psalmopoeus irminia",
-    sex: "Samice",
-    size: "3.5 cm",
-    price: "700 Kč",
-
-    continent: "Amerika"
-  },
-
-  {
-    name: "Psalmopoeus reduncus",
-    sex: "Samice",
-    size: "adult",
-    price: "800 Kč",
-    
-    continent: "Amerika"
-  },
-
-  {
-    name: "Pterinochilus lugardi",
-    sex: "Samice",
-    size: "adult",
-    price: "1200 Kč",
-    
-    continent: "Afrika"
-  },
-
-  {
+{
     name: "Heterothele gabonensis",
     sex: "Samice",
     size: "adult",
     price: "800 Kč",
-    
     continent: "Afrika"
-  },
+},
 
-  {
-    name: "Heteroscodra maculata",
+{
+    name: "Phormictopus sp. Green",
     sex: "Samice",
-    size: "3 cm",
-    price: "700 Kč",
-    slug: "heteroscodra-maculata",
-    continent: "Afrika"
-  },
-  {
-    name: "Poecilotheria ornata",
-    sex: "Samice",
-    size: "adult",
-    price: "1700 Kč",
+    size: "5.5 cm",
+    price: "1000 Kč",
+    continent: "Amerika"
+},
 
-    continent: "Asie"
-  },
-  {
+{
     name: "Poecilotheria regalis",
     sex: "Samice",
     size: "5 cm",
     price: "1500 Kč",
-    
     continent: "Asie"
-  },
-  {
+},
+
+{
+    name: "Psalmopoeus irminia",
+    sex: "Samice",
+    size: "3.5 cm",
+    price: "700 Kč",
+    continent: "Amerika"
+},
+
+{
+    name: "Psalmopoeus reduncus",
+    sex: "Samice",
+    size: "adult",
+    price: "800 Kč",
+    continent: "Amerika"
+},
+
+{
+    name: "Pterinochilus lugardi",
+    sex: "Samice",
+    size: "adult",
+    price: "1200 Kč",
+    continent: "Afrika"
+},
+
+{
     name: "Pterinochilus murinus RCF",
     sex: "Samice",
     size: "adult",
     price: "500 Kč",
     slug: "pterinochilus-murinus-rcf",
     continent: "Afrika"
-  },
-  {
-    name: "Psalmopoeus cambridgei",
-    sex: "Samice",
-    size: "4 cm",
-    price: "700 Kč",
-    slug: "psalmopoeus-cambridgei",
+},
+
+
+
+
+{
+    name: "Avicularia laeta",
+    sex: "Samec",
+    size: "adult",
+    price: "1500 Kč",
     continent: "Amerika"
-  },
+},
 
+{
+    name: "Brachypelma hamorii",
+    sex: "Samec",
+    size: "sub-adult",
+    price: "1000 Kč",
+    continent: "Amerika"
+},
 
-
-
-  {
+{
     name: "Chilobrachys sp. Vietnam Blue",
     sex: "Samec",
     size: "adult",
     price: "400 Kč",
-    
     continent: "Asie"
-  },
+},
 
-  {
-    name: "Psalmopoeus cambridgei",
-    sex: "Samec",
-    size: "sub-adult",
-    price: "400 Kč",
-    
-    continent: "Amerika"
-  },
-
-  {
+{
     name: "Psalmopoeus reduncus",
     sex: "Samec",
     size: "4 cm",
     price: "400 Kč",
-    
     continent: "Asie"
-  },
+},
 
 
 
 
 
 
-      {
-    name: "Ornithoctonus aureotibialis",
-    sex: "Neurčené",
-    size: "1.svlek",
-    price: "100 Kč/kus",
-
-    continent: "Asie"
-  },
-
-  {
+{
     name: "Chilobrachys sp. Vietnam Blue",
     sex: "Neurčené",
     size: "1.svlek",
     price: "80 Kč/kus",
-    
     continent: "Asie"
-  },
+},
 
-
-
-
-
-
-  {
-    name: "Cyriopagopus sp. Hati Hati",
-    sex: "Neurčené",
-    size: "2 cm",
-    price: "200 Kč/kus",
-    
-    continent: "Asie"
-  },
-
-  {
+{
     name: "Heteroscodra maculata",
     sex: "Neurčené",
     size: "5. slvek",
     price: "150 Kč/kus",
-    
     continent: "Afrika"
-  },
+},
+
+{
+    name: "Ornithoctonus aureotibialis",
+    sex: "Neurčené",
+    size: "1.svlek",
+    price: "100 Kč/kus",
+    continent: "Asie"
+},
 
 
 
